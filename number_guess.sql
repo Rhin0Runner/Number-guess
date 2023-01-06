6eebd450ffba19c0a67bdb5c73afb787864fc8da
Rhin0Runner <98430637+Rhin0Runner@users.noreply.github.com>
2023-01-06 09:31:16 -0700

    Create number_guess.sql

---
+++ number_guess.sql
@@ -0,0 +1,116 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+-- Dumped by pg_dump version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE number_guess;
+--
+-- Name: number_guess; Type: DATABASE; Schema: -; Owner: freecodecamp
+--
+
+CREATE DATABASE number_guess WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE number_guess OWNER TO freecodecamp;
+
+\connect number_guess
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.users (
+    username character varying(22),
+    games_played integer,
+    best_game integer,
+    user_id integer NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO freecodecamp;
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.users_user_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_user_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.users_user_id_seq OWNED BY public.users.user_id;
+
+
+--
+-- Name: users user_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.users_user_id_seq'::regclass);
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.users VALUES ('user_1672951532045', 2, 1000, 53);
+INSERT INTO public.users VALUES ('user_1672951532046', 5, 1000, 52);
+INSERT INTO public.users VALUES ('user_1672951652067', 2, 1000, 55);
+INSERT INTO public.users VALUES ('user_1672951652068', 5, 1000, 54);
+INSERT INTO public.users VALUES ('Israel', 1, 1000, 56);
+INSERT INTO public.users VALUES ('user_1672952056566', 2, 1000, 58);
+INSERT INTO public.users VALUES ('user_1672952056567', 5, 1000, 57);
+INSERT INTO public.users VALUES ('Gillary', 4, 1000, 51);
+INSERT INTO public.users VALUES ('user_1672952265563', 2, 1000, 60);
+INSERT INTO public.users VALUES ('user_1672952265564', 5, 1000, 59);
+INSERT INTO public.users VALUES ('Logan', 5, 1000, 50);
+
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.users_user_id_seq', 60, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
